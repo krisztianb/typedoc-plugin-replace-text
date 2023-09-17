6c2e51f082e659b9d2944d1e032877f4ca9c9b31
Krisztian Balla <k.balla@gmx.at>
2023-09-17 11:55:47 +0200

    Fix error message in test script

--- test/test.js
+++ test/test.js
@@ -11,7 +11,7 @@ if (!fs.existsSync("..\\dist")) {
 
 console.log("Copying current build of plugin to node_modules for testing...");
 
-fs.rm("..\\node_modules\\typedoc-plugin-replace-text", { recursive: true, force: true });
+fs.rm("..\\node_modules\\typedoc-plugin-replace-text", { recursive: true, force: true }, () => {});
 fs.mkdirSync("..\\node_modules\\typedoc-plugin-replace-text\\dist", { recursive: true });
 fs.copyFileSync("..\\package.json", "..\\node_modules\\typedoc-plugin-replace-text\\package.json");
 fs.cpSync("..\\dist", "..\\node_modules\\typedoc-plugin-replace-text\\dist", { recursive: true });
