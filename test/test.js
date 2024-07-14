695c6a39baf7ae2a05343eadf5b428bdaaf19920
Krisztian Balla <k.balla@gmx.at>
2024-07-14 19:02:19 +0200

    Fix synchronous file system operations not working in tests

--- test/test.js
+++ test/test.js
@@ -1,22 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { execSync } = require("child_process");
-const fs = require("fs");
-
-console.log("=================================== SETTING UP THE TESTS ===========================================");
-
-if (!fs.existsSync("..\\dist")) {
-    console.error("ERROR: Cannot find 'dist' folder. Did you forget to build the plugin with 'npm run build'?");
-    process.exit(1);
-}
-
-console.log("Copying current build of plugin to node_modules for testing...");
-
-fs.rm("..\\node_modules\\typedoc-plugin-replace-text", { recursive: true, force: true }, () => {});
-fs.mkdirSync("..\\node_modules\\typedoc-plugin-replace-text\\dist", { recursive: true });
-fs.copyFileSync("..\\package.json", "..\\node_modules\\typedoc-plugin-replace-text\\package.json");
-fs.cpSync("..\\dist", "..\\node_modules\\typedoc-plugin-replace-text\\dist", { recursive: true });
-
-console.log("DONE\n");
 
 console.log("============================= REPLACE ONLY IN CODE COMMENT TEXT ====================================");
 execSync("npx typedoc --options replace-only-in-code-comment-text/typedoc.json", { stdio: "inherit" });
