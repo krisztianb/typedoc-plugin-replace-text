44e0d8c0ade862db9db54fda658f4543bf32b883
Krisztian Balla <k.balla@gmx.at>
2024-12-09 11:13:56 +0100

    Remove unused files

--- test/replace-in-markdown/output/assets/navigation.js
+++
@@ -1,1 +0,0 @@
-window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAACo2PPQ+CMBRF/8ubCxg1pOkG4mBi3JwIIf0yNEBrpCQSw383HQDRDs7vnXvvyV9g5dMCgdPlcL5mxwwQ3KmtgIAwvG+ltl003cLKtg0gqJUWQPAO43iDRzRn0AVujegb2UV0zWwR8Eo14iE1kHwGkwW89ZpbZbRDkzUc7/30NC9gQ0B9+13S9FSyoaR+kWIsPmTYrwz7Syb1ybAw/ZZxfW+yAUiQgAEAAA=="
